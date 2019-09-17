d369924167ee4c52eb0c42d008c304fdf4c7edc4
nrminar <nrminar@gmail.com>
2019-09-17 15:35:07 -0500

    finished pairs project

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
 class RockCounter extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      rocksPicked: 0,
+    }   
+  }
+  Increase = (event) =>{
+    console.log('increase')
+    if(this.state.rocksPicked < 50){
+      this.setState({ rocksPicked: this.state.rocksPicked + 1})
+    }else if(this.state.rocksPicked === 50){
+      this.setState({ rocksPicked: this.state.rocksPicked + '   Done'})
+    }
+  }
+  Decrease = (event) =>{
+    console.log('decrease');
+    if(this.state.rocksPicked > 0){
+      this.setState({ rocksPicked: this.state.rocksPicked - 1})
+    }
+  }
+  Reset = () =>{
+    console.log('reset');
+    this.setState({ rocksPicked: 0})
+  }
   render() {
     return (
       <div>
           <div>
-              Rocks Picked: 0
+              Rocks Picked: {this.state.rocksPicked}
           </div>
           <div>
-            <button>Increase</button>
-            <button>Decrease</button>
-            <button>Reset</button>
+            <button onClick = {this.Increase}>Increase</button>
+            <button onClick = {this.Decrease}>Decrease</button>
+            <button onClick = {this.Reset}>Reset</button>
           </div>
       </div>
     );
